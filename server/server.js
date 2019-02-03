730a4b33be889995b8eeb6f0a49e5cc8d4779a4d
kartikchoubey <chaubeykartik@gmail.com>
2019-02-03 16:09:30 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
  const path =require("path");
  const express=require('express');
+ const port = process.env.PORT || 3000;
 
  const publicPath = path.join(__dirname,'../public');
 var app=express();
@@ -7,6 +8,6 @@ var app=express();
 app.use(express.static(publicPath));
 
 
-app.listen(3000,()=>{
-    console.log('Started on port 3000');
+app.listen(port,()=>{
+    console.log(`Started on port ${port }`);
 })
